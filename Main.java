d95b792850f9342b0603eea3cac37f1e6ddf2549
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-11-15 15:25:56 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
     public static void main(String[] args) {
-        PersonalAccount account = new PersonalAccount(7777, "Jasmin and Saikal");
+        try {
+            // Creating a PersonalAccount object
+            PersonalAccount account = new PersonalAccount(7777, "Jasmin and Saikal");
 
-        account.deposit(1000);
-        account.withdraw(500);
-        account.deposit(200);
-        account.withdraw(300);
-        account.withdraw(500);
+            // Performing transactions
+            account.deposit(1000);
+            account.withdraw(500);
+// This should throw InsufficientBalanceException
 
-        account.displayTransactions();
+            // Displaying transactions
+            account.displayTransactions();
+        } catch (InsufficientBalanceException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
